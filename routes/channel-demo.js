38cf6a65ce11d6062778df4e4da00e82b5780e08
Heechan <heechan@Heechanui-MacBookPro.local>
2024-09-25 15:28:07 +0900

    JWT 적용 6주차 파트3

--- routes/channel-demo.js
+++ routes/channel-demo.js
@@ -2,42 +2,58 @@ const express = require('express')
 const router = express.Router()
 router.use(express.json())
 const conn = require('../db')
+const { param, body, validationResult } = require('express-validator');
 
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Bad request', errors: errors.array() });
+    }
+    next();
+}
 
 router.route('/channels')
-    .post(function (req, res) {
-        let { name, desc, user_id } = req.body
+    .post(
+        [
+            body('user_id').notEmpty().isInt().withMessage('user_id must be integer'),
+            body('name').notEmpty().isString().withMessage('name must be string'),
+            validate
+        ],
+        function (req, res) {
 
-        conn.query(`SELECT * FROM channels WHERE name = ?`, name, function (err, results) {
-            if (results.length) {
-                res.status(409).json({
-                    message: 'Channel already exists'
-                })
-            } else {
-                
+            let { name, user_id } = req.body
 
-
-                conn.query(`INSERT INTO channels (name, desc, user_id) VALUES (?, ?, ?)`, [name, desc, user_id],
-                    function (err, results, fields) {
-                        res.status(201).json({
-                            message: `Channel ${name} created`
+            conn.query(`INSERT INTO channels (name, user_id) VALUES (?, ?)`, [name, user_id],
+                function (err, results) {
+                    if (err) {
+                        res.status(400).json({
+                            message: 'Bad request'
                         })
                     }
-                )
-            }
-        })
+                    res.status(201).json({
+                        message: `Channel ${name} created`,
+                        info: results
+                    })
+                }
+            )
+        }) //channel create
+    .get(
+        [
+            body('user_id').notEmpty().isInt().withMessage('user_id must be integer'),
+            validate
+        ], function (req, res) {
 
+            var { user_id } = req.body
 
-
-
-    }) //channel create
-    .get(function (req, res) {
-
-        var { user_id } = req.body
-
-        if (user_id) {
             conn.query(`SELECT * FROM channels WHERE user_id = ?`, user_id,
                 function (err, results) {
+                    if (err) {
+                        res.status(400).json({
+                            message: 'Bad request'
+                        })
+                    }
+
                     if (results.length) {
                         res.status(200).json(results)
                     } else {
@@ -45,82 +61,98 @@ router.route('/channels')
                     }
                 }
             )
-        } else {
-            res.status(400).json({
-                message: 'Bad request'
-            }).end()
-        }
-        
 
-        
-
-    }) //channel list
+        }) //channel list
 
 router.route('/channel/:id')
-    .delete(function (req, res) {
-        let {channel_id }= req.params
+    .delete(
+        [
+            param('id').notEmpty().isInt().withMessage('channel id required in number form'),
+            validate
+        ], (req, res) => {
 
-        conn.query(`SELECT * FROM channels WHERE id = ?`, channel_id, 
-            function (err, results) {
-                if (results.length) {
-                    conn.query(`DELETE FROM channels WHERE id = ?`, id,
-                        function (err, results) {
-                            res.status(200).json({
-                                message: 'Channel deleted'
-                            })
-                        }
-                    )
-                } else {
-                    notFoundchannel(res)
+            let { id } = req.params
+            console.log(id)
+
+            conn.query(`SELECT * FROM channels WHERE id = ?`, id,
+                function (err, results) {
+                    if (results.length) {
+                        conn.query(`DELETE FROM channels WHERE id = ?`, id,
+                            function (err, results) {
+                                res.status(200).json({
+                                    message: 'Channel deleted',
+                                    info: results
+                                })
+                            }
+                        )
+                    } else {
+                        notFoundchannel(res)
+                    }
                 }
-            }
-        )
-    }) //single channel delete 
-    .get(function (req, res) {
-        let { id } = req.params
-        id = parseInt(id)
+            )
+        }) //single channel delete 
+    .get(
+        [
+            param('id').notEmpty().isInt().withMessage('channel id required in number form'),
+            validate
+        ], (req, res) => {
 
-        conn.query(`SELECT * FROM channels WHERE id = ?`, id,
-            function (err, results) {
+            let { id } = req.params
 
-                if (results.length) {
-                    res.status(200).json(results)
-                } else {
-                    notFoundchannel(res)
+            conn.query(`SELECT * FROM channels WHERE id = ?`, id,
+                function (err, results) {
+
+                    if (err) {
+                        return res.status(400).json({
+                            message: 'Bad request no channel found'
+                        })
+                    }
+
+                    if (results.length) {
+                        res.status(200).json(results)
+                    } else {
+                        return notFoundchannel(res)
+                    }
                 }
-            }
-        )
+            )
 
-    }) //channle detail
-    .put(function (req, res) {
-        let { id } = req.params
-        id = parseInt(id)
+        }) //channle detail
+    .put(
+        [
+            param('id').notEmpty().isInt().withMessage('channel id required in number form'),
+            validate
+        ], (req, res) => {
 
-        var channel = db.get(id)
-        console.log("channel name", channel)
 
-        var oldName = channel.name
 
-        if (channel) {
-            var newName = req.body.name
-            channel.name = newName
+            let { id } = req.params
 
-            db.set(id, channel)
-            res.status(200).json({
-                message: `Channel ${oldName} modified to ${newName}`
-            })
-        } else {
-            res.status(400).json({
-                message: 'Bad request'
-            })
-        }
-    }) //channel modify
+            conn.query(`SELECT * FROM channels WHERE id = ?`, id,
+                function (err, results) {
+                    if (results.length) {
+                        let { name } = req.body
+                        conn.query(`UPDATE channels SET name = ? WHERE id = ?`, [name, id],
+                            function (err, results) {
+                                res.status(200).json({
+                                    message: 'Channel modified',
+                                    info: results
+                                })
+                            }
+                        )
+                    } else {
+                        notFoundchannel(res)
+                    }
+                }
+            )
+
+        }) //channel modify
 
 function notFoundchannel(res) {
     res.status(404).json({
         message: 'Channel not found'
     })
 }
+
 
 module.exports = router
 
--- routes/user-demo.js
+++ routes/user-demo.js
@@ -2,99 +2,165 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json())
 const conn = require('../db')
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+var cookieParser = require('cookie-parser')
+router.use(cookieParser())
 
 
-router.post('/login', function (req, res) {
 
-    const { email, password } = req.body
 
-    conn.query(`SELECT * FROM users WHERE email = ?`, [email],
-        function (err, results) {
 
-            loginUser = results[0]
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
 
-            if (loginUser && loginUser.password === password) {
-                res.status(200).json({
-                    message: 'Login success'
-                })
-            } else if (loginUser && loginUser.password !== password) {
-                res.status(400).json({
-                    message: 'Password is incorrect'
-                })
-            } else {
-                res.status(404).json({
-                    message: 'User not found'
-                })
-            }
-        }
-    )
-
-    
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Bad request', errors: errors.array() });
+    }
+    next();
 }
+const createEmailChain = () => body('email').notEmpty().isEmail().withMessage('email must be email')
+const createPasswordChain = () => body('password').notEmpty().isString().withMessage('password must be string')
+const createNameChain = () => body('name').notEmpty().isString().withMessage('name must be string')
+const createContactChain = () => body('contact').notEmpty().isString().withMessage('contact must be string')
+
+
+router.post('/login',
+    [
+        createEmailChain(),
+        createPasswordChain(),
+        validate
+    ], (req, res) => {
+
+        const { email, password } = req.body
+
+        conn.query(`SELECT * FROM users WHERE email = ?`, [email],
+            function (err, results) {
+
+                loginUser = results[0]
+
+                if (loginUser && loginUser.password === password) {
+
+                    const token = jwt.sign({ 
+                        email: loginUser.email,
+                        name: loginUser.name,
+                        tokexpiresIn: '30m', 
+                        issuer: 'heechan'
+                    
+                    }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+                    res.cookie("token", token, {
+                        httpOnly: true,
+                        secure: false,
+                        sameSite: 'none'
+                    })
+
+                    res.status(200).json({
+                        message: 'Login success',
+                        token
+                    })
+                } else if (loginUser && loginUser.password !== password) {
+                    res.status(400).json({
+                        message: 'Password is incorrect'
+                    })
+                } else {
+                    res.status(404).json({
+                        message: 'User not found'
+                    })
+                }
+            }
+        )
+
+
+    }
 )
 
-router.post('/join', function (req, res) {
-    let { email, password, name, contact } = req.body
+router.post('/join',
+    [
+        createEmailChain(),
+        createPasswordChain(),
+        createNameChain(),
+        createContactChain(),
+        validate
+    ]
+    , (req, res) => {
 
-    conn.query(
-        `SELECT * FROM users WHERE email = ?`, email,
-        function (err, results) {
-            if (results.length > 0) {
-                res.status(409).json({
-                    message: 'User already exists'
-                })
-            } else {
-                conn.query(`INSERT INTO users (email, password, name, contact) VALUES (?, ?, ?, ?)`,
-                    [email, password, name, contact],
-                    function (err, results, fields) {
-                        res.status(201).json({
-                            message: 'Join success'
-                        })
-                    })
-            }
-        }
-    );
-
-
-})
-
-//조회
-router.route('/user')
-    .get(function (req, res) {
-
-        let { email, name } = req.body
-        console.log(email)
-        console.log(name)
+        let { email, password, name, contact } = req.body
+        console.log(email, password, name, contact)
 
         conn.query(
             `SELECT * FROM users WHERE email = ?`, email,
             function (err, results) {
-                if (results) {
-                    console.log(results)
-                    res.json(results)
+                if (results.length > 0) {
+                    res.status(409).json({
+                        message: 'User already exists'
+                    })
                 } else {
-                    console.log('error')
+                    conn.query(`INSERT INTO users (email, password, name, contact) VALUES (?, ?, ?, ?)`,
+                        [email, password, name, contact],
+                        function (err, results) {
+                            if (err) {
+                                return res.status(400).json({
+                                    message: 'Bad request'
+                                })
+                            }
+                            res.status(201).json({
+                                message: 'Join success',
+                                info: results
+                            })
+                        })
                 }
             }
         );
 
 
     })
-    .delete(function (req, res) {
 
-        let { email } = req.body
+//조회
+router.route('/user')
+    .get(
+        [
+            createEmailChain(),
+            validate
+        ], (req, res) => {
 
-        conn.query(`DELETE FROM users Where email = ?`, email,
-            function( err, results) {
-                if (results.affectedRows > 0) {
-                    res.status(200).json({
-                        message: `User ${email} deleted`
-                    })
-                } else {
-                    res.status(404).json(`User ${email} not found`)
+            let { email } = req.body
+
+            conn.query(
+                `SELECT * FROM users WHERE email = ?`, email,
+                function (err, results) {
+                    if (results) {
+                        console.log(results)
+                        res.json(results)
+                    } else {
+                        console.log('error')
+                    }
                 }
-            }
-        )
-    })
+            );
+
+
+        })
+    .delete(
+        [
+            createEmailChain(),
+            validate
+        ], (req, res) => {
+
+            let { email } = req.body
+
+            conn.query(`DELETE FROM users Where email = ?`, email,
+                function (err, results) {
+                    if (results.affectedRows > 0) {
+                        res.status(200).json({
+                            message: `User ${email} deleted`,
+                            info: results
+                        })
+                    } else {
+                        res.status(404).json(`User ${email} not found`)
+                    }
+                }
+            )
+        })
 
 module.exports = router;
